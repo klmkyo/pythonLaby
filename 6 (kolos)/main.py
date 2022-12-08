a67be41ff179df1ca35ec8280e136f77e6dbb59f
klmkyo <mail@mklimek.dev>
2022-12-08 13:26:45 +0100

    Improvement

--- 6 (kolos)/main.py
+++ 6 (kolos)/main.py
@@ -1,34 +1,36 @@
 def schemat_hornera(x, lista_a: list[float]):
-    """ n-ty stopien jest wyczytywany z długości listy a """
+    """ n-ty stopien jest wyczytywany z długości lista_a """
     wyn = lista_a[0]
 
-    for i in range(0, len(lista_a) - 1):
+    for i in range(1, len(lista_a)):
         wyn = wyn*x + lista_a[i]
 
     return wyn
 
 def pochodna_schemat_hornera(x, lista_a: list[float]):
-    """ n-ty stopien jest wyczytywany z długości listy a """
+    """ n-ty stopien jest wyczytywany z długości lista_a """
     wyn = lista_a[0]
 
-    for i in range(0, len(lista_a) - 2):
+    for i in range(1, len(lista_a) - 1):
         wyn = wyn*x*(i+1) + lista_a[i]
         
     return wyn
 
 def miejsce_zerowe(start, lista_a: list[float], epsilon=0.0001):
     xn = start
-
-    while abs(xn) >= epsilon:
+    # setting roznica to epsilon + 1 to enter the loop
+    roznica = epsilon + 1
+    
+    while abs(roznica) >= epsilon:
         print(xn)
         gora = schemat_hornera(xn, lista_a)
         pochodna = pochodna_schemat_hornera(xn, lista_a)
         if pochodna == 0: 
             xn = (xn + gora)/2
             continue
-
-        xn = xn - (gora/pochodna)
+        roznica = (gora/pochodna)
+        xn = xn - roznica
 
     return xn 
 
-print(miejsce_zerowe(-2, [1,-2,-3]))
+print(miejsce_zerowe(-2, [2,-2,-3]))
